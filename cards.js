21f23a747d26747360043587578c3230ad70107f
rbariyal <rbariyal64@gmail.com>
2023-01-29 10:47:27 +0530

    first commit

---
+++ cards.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+
+
+const CardSchema=mongoose.Schema(
+    {
+        card:String,
+cardHolder:String,
+        expiry:String,
+    }
+)
+
+module.exports=mongoose.model('cards',CardSchema);
